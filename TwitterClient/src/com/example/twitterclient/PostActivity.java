ae081f467a1ffdcd7f648eea2e31fdb2d43deb34
Vishal Kapoor <vkapoor@zynga.com>
2014-02-02 21:54:28 -0800

    Tweet characters remaining

--- TwitterClient/src/com/example/twitterclient/PostActivity.java
+++ TwitterClient/src/com/example/twitterclient/PostActivity.java
@@ -5,6 +5,8 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -13,28 +15,65 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.twitterclient.models.Tweet;
+import com.example.twitterclient.util.EndlessScrollListener;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class PostActivity extends Activity {
-	EditText etTweet;
-	TextView tvUser;
-	ImageView ivUserProfile;
+	
+	public static final String LOG_TAG = TimelineActivity.class.getName();
+
+    // Views
+	private EditText etTweet;
+	private TextView tvUser;
+	private ImageView ivUserProfile;
+	private TextView tvRemainingCharacters;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post);
-		etTweet = (EditText) findViewById(R.id.etTweet);
-		tvUser = (TextView) findViewById(R.id.tvUser);
-		ivUserProfile = (ImageView) findViewById(R.id.ivUserProfile);
-		
+
+        setupViews();
+
 		String screenName = getIntent().getStringExtra("screenName");
 		String profileImageUrl = getIntent().getStringExtra("profileImageUrl");
 		
 		tvUser.setText('@'+screenName);
 		ImageLoader.getInstance().displayImage(profileImageUrl, ivUserProfile);
+		
+        addListeners();
+
 	}
+    
+    private void setupViews() {
+		etTweet = (EditText) findViewById(R.id.etTweet);
+		tvUser = (TextView) findViewById(R.id.tvUser);
+		ivUserProfile = (ImageView) findViewById(R.id.ivUserProfile);
+		tvRemainingCharacters = (TextView) findViewById(R.id.tvRemainingChars);
+    }
+
+    private void addListeners() {
+    	etTweet.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            	tvRemainingCharacters.setHint(
+        			String.valueOf(
+    					getResources().getInteger(
+							R.integer.tweet_max_length) - etTweet.getText().toString().length()));
+            }
+        });
+    }
+    
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
