2ff96e5088500087cc4c9c509915bdd8988c605e
Vishal Kapoor <vkapoor@zynga.com>
2014-02-04 03:04:25 -0800

    Remove extra import

--- RottenTomatoesDemo/src/com/example/rottentomatoesdemo/BoxOfficeActivity.java
+++ RottenTomatoesDemo/src/com/example/rottentomatoesdemo/BoxOfficeActivity.java
@@ -8,7 +8,6 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
 import android.widget.ListView;
