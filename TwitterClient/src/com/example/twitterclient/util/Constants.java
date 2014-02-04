1271c49d979cc15e2424545de71e6de088f0a97c
Vishal Kapoor <vkapoor@zynga.com>
2014-02-04 03:12:09 -0800

    Constants update

--- TwitterClient/src/com/example/twitterclient/util/Constants.java
+++ TwitterClient/src/com/example/twitterclient/util/Constants.java
@@ -1,6 +1,6 @@
 package com.example.twitterclient.util;
 
 public class Constants {
-    public static final int httpTimeoutMilliSeconds = 300;
+    public static final int httpCallTimeoutMilliSeconds = 300;
 	public static final int tweetsQueryLimit = 20;
 }
