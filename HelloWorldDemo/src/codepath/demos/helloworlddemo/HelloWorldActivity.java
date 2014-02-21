5c53b390f017588584adc3a75cc9331cb9c3b30a
Vishal Kapoor <vkapoor@zynga.com>
2014-02-20 23:27:22 -0800

    Code to get key hashes for Android Applications

--- HelloWorldDemo/src/codepath/demos/helloworlddemo/HelloWorldActivity.java
+++ HelloWorldDemo/src/codepath/demos/helloworlddemo/HelloWorldActivity.java
@@ -1,7 +1,16 @@
 package codepath.demos.helloworlddemo;
 
-import android.os.Bundle;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 
 public class HelloWorldActivity extends Activity {
@@ -10,6 +19,23 @@ public class HelloWorldActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hello_world);
+
+		// Code to get key hash for FB applications 
+		String packageName = "com.hush";
+		try {
+            PackageInfo info = getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                }
+        } catch (NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+		
+		return;
 	}
 
 	@Override
