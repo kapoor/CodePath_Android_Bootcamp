97ffdec0022a00eb984858662718fa91b750ca36
Vishal Kapoor <vkapoor@zynga.com>
2014-02-24 19:38:23 -0800

    NavigationDrawerDemo in class

---
+++ NavigationDrawerDemo/src/com/example/navigationdrawerdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.navigationdrawerdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+		fts.replace(R.id.frameLayout, new FirstFragment());
+		fts.commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ NavigationDrawerDemo/src/com/example/navigationdrawerdemo/ThirdFragment.java
@@ -0,0 +1,7 @@
+package com.example.navigationdrawerdemo;
+import android.support.v4.app.Fragment;
+
+
+public class ThirdFragment extends Fragment {
+
+}
